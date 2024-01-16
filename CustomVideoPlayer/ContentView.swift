35b8c866bbdd7f177af0ed4dd531521757c7a75b
shenxiang <shenxiang@foursmile001.com>
2024-01-16 13:00:26 +0800

    Initial Commit

---
+++ CustomVideoPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomVideoPlayer
+//
+//  Created by FS on 2024/1/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomVideoPlayer/CustomVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoPlayerApp.swift
+//  CustomVideoPlayer
+//
+//  Created by FS on 2024/1/16.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
